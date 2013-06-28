47679114e1f0677ed4ac88ce863a69b1462a2fe4
Brandon <bmaxwell921@gmail.com>
2013-06-28 13:38:41 -0700

    Added image for Dirk

--- PixHell/src/com/theoc/pixhell/model/GameObject.java
+++ PixHell/src/com/theoc/pixhell/model/GameObject.java
@@ -1,6 +1,7 @@
 package com.theoc.pixhell.model;
 
 
+import android.graphics.Bitmap;
 import android.graphics.Point;
 
 public abstract class GameObject {
@@ -9,6 +10,7 @@ public abstract class GameObject {
 	public Point velocity;
 	public float height;
 	public float width;
+	public Bitmap image;
 	
 	public abstract void update(float time);
 	public abstract boolean CollidesWith(GameObject gameObject);
