8b90a833e10646ef7f19f21b8a04e13531b7e91c
Dirker27 <hortensd@gmail.com>
2013-06-28 12:43:10 -0700

    Added Game Hierarchy
    Game Activity
    Game View
    Model Package

---
+++ PixHell/src/com/theoc/pixhell/GameView.java
@@ -0,0 +1,42 @@
+package com.theoc.pixhell;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public final class GameView extends View
+	implements Observer
+{
+	
+
+	public GameView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+	}
+	
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		boolean b = false;
+		
+		return b;
+	}
+	
+	@Override
+	public void onDraw(Canvas canvas) {
+		
+	}
+
+
+	@Override
+	public void update(Observable observable, Object data)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ PixHell/src/com/theoc/pixhell/model/LevelObject.java
@@ -0,0 +1,8 @@
+package com.theoc.pixhell.model;
+
+import java.util.Observable;
+
+public class LevelObject extends Observable
+{
+
+}
