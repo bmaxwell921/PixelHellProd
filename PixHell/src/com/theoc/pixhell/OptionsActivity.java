e116a1cfd382c7cab6099c37b55f488207575f77
rohan joshi <brel_rj@yahoo.com>
2013-06-28 13:18:16 -0700

    changed UI for options and store activity

--- PixHell/src/com/theoc/pixhell/OptionsActivity.java
+++ PixHell/src/com/theoc/pixhell/OptionsActivity.java
@@ -1,15 +1,26 @@
 package com.theoc.pixhell;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class OptionsActivity extends Activity {
+
+	ListView lv;
+	String menuOptions[] = { "Tilt Sensitivity", "Wallet", "Difficulty",
+			"Weapon Cache" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_options);
+
+		lv = (ListView) findViewById(R.layout.activity_options);
+		lv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, menuOptions));
+
 	}
 
 	@Override
--- PixHell/src/com/theoc/pixhell/StoreActivity.java
+++ PixHell/src/com/theoc/pixhell/StoreActivity.java
@@ -3,13 +3,23 @@ package com.theoc.pixhell;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class StoreActivity extends Activity {
+
+	ListView lv;
+	String menuOptions[] = { "Tilt Sensitivity", "Wallet", "Difficulty",
+			"Weapon Cache" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_store);
+
+		lv = (ListView) findViewById(R.layout.activity_options);
+		lv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, menuOptions));
 	}
 
 	@Override
