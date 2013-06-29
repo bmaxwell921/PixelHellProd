7bf653cab3eafc76e937246fa40ab46423a59afa
unknown <jssharma@SEA-1201012414.ant.amazon.com>
2013-06-29 00:25:55 -0700

    ship fire method

--- PixHell/src/com/theoc/pixhell/model/Ship.java
+++ PixHell/src/com/theoc/pixhell/model/Ship.java
@@ -13,54 +13,50 @@ public abstract class Ship extends GameObject {
 	private static final int shipWidth = 100;
 	private static final int shipHeight = 100;
 	public float FiringTime;
-	
+
 	private StatInfo stats;
-	
-	
+
 	public Ship(Bitmap image) {
 		this(image, defaultFireRate);
-		
+
 	}
-	
+
 	public Ship(Bitmap image, float fireRate) {
 		super(image);
 		stats = new StatInfo(defaultHealth, defaultDamage, fireRate);
-		this.FiringTime=this.stats.getCurFireRate();
-		
+		this.FiringTime = this.stats.getCurFireRate();
+
 	}
-	
+
 	public Ship(Bitmap image, Vector2 location, Vector2 maxVel) {
 		this(image, location, maxVel, defaultFireRate);
-		
+
 	}
-	
+
 	public Ship(Bitmap image, Vector2 location, Vector2 maxVel, float fireRate) {
 		super(location, maxVel, shipHeight, shipWidth, image);
 		stats = new StatInfo(defaultHealth, defaultDamage, fireRate);
-		this.FiringTime=this.stats.getCurFireRate();
-		
+		this.FiringTime = this.stats.getCurFireRate();
+
 	}
-	
+
 	public void applyDamage(int damage) {
 		stats.changeHealth(-1 * damage);
 	}
-	
-	public BulletWeapon Fire(float time)
-	{
-		if(this.FiringTime < 0)
-		{
-		this.stats.resetFireRate();
-		this.FiringTime=this.stats.getCurFireRate();
-		BulletWeapon bullet = new BulletWeapon(new Vector2(this.position.x+1,this.position.y),new Vector2(1,1), 1, 1,AssetMap.getImage(AssetMap.shot));
-		return bullet;
+
+	public Weapon Fire(float time) {
+		if (this.FiringTime < 0) {
+
+			this.FiringTime = this.stats.getCurFireRate();
+			Weapon bullet = new Weapon(new Vector2(
+					this.position.x + 1, this.position.y), new Vector2(1, 1),
+					1, 1, AssetMap.getImage(AssetMap.shot));
+			return bullet;
+		} else {
+			this.FiringTime = this.FiringTime - time;
+			return null;
 		}
-		else
-		{
-		this.FiringTime=this.FiringTime-100;
-		return null;
-		}
-		
+
 	}
-	
-	
+
 }
