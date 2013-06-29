f66982f66ad80e55177cbfbb838b4750db210704
Brandon <bmaxwell921@gmail.com>
2013-06-29 10:34:59 -0700

    Starting impl for circle blaster

--- PixHell/src/com/theoc/pixhell/model/LevelObject.java
+++ PixHell/src/com/theoc/pixhell/model/LevelObject.java
@@ -17,6 +17,7 @@ import com.theoc.pixhell.utilities.Difficulty;
 import com.theoc.pixhell.utilities.GameState;
 import com.theoc.pixhell.utilities.WeaponType;
 import com.theoc.pixhell.weaponLaunchers.BulletLauncher;
+import com.theoc.pixhell.weaponLaunchers.CircleBlaster;
 import com.theoc.pixhell.weaponLaunchers.MissileLauncher;
 import com.theoc.pixhell.weaponLaunchers.TriBlasterLauncher;
 
@@ -64,7 +65,7 @@ public class LevelObject extends Observable {
 		player = new Player(AssetMap.getImage(AssetMap.playerOne), im,
 				screenWidth, screenHeight, 100);
 
-		this.setPlayerWeapon(WeaponType.BULLET);
+		this.setPlayerWeapon(WeaponType.CIRCLE_BLASTER);
 		
 
 		Random randomGenerator = new Random();
@@ -122,6 +123,9 @@ public class LevelObject extends Observable {
 					.getMissileDamage(), player.stats.getMissileCooldown()));
 		} else if (weapon == WeaponType.TRI_BLASTER) {
 			player.addLauncher(new TriBlasterLauncher(player.stats.getBulletDamage(), 
+					player.stats.getBulletCooldown()));
+		} else if (weapon == WeaponType.CIRCLE_BLASTER) {
+			player.addLauncher(new CircleBlaster(player.stats.getBulletDamage(), 
 					player.stats.getBulletCooldown()));
 		}
 	}
