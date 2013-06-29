08e6f60e2a16d234d216e9a7a6125935a9720110
unknown <jssharma@SEA-1201012414.ant.amazon.com>
2013-06-29 04:23:54 -0700

    Explosion class

--- PixHell/src/com/theoc/pixhell/model/Explosion.java
+++ PixHell/src/com/theoc/pixhell/model/Explosion.java
@@ -1,16 +1,24 @@
 package com.theoc.pixhell.model;
 
-public class Explosion {
+import com.theoc.pixhell.logic.AssetMap;
+import com.theoc.pixhell.utilities.Vector2;
+
+public class Explosion extends GameObject {
 	public final int LifeTime =10000;
 	public int Timeleft;
 	public boolean isAlive;
 	public final int MilliSeconds =500;
+	public Vector2 position;
 	
 	
-	public Explosion() {
-		super();
-		Timeleft =LifeTime;
+	
+	public Explosion(Vector2 position){
+		super(AssetMap.getImage(AssetMap.explosion),100,100);
+		this.position=position;
+		
 	}
+	
+	
 
 
 	public void Update(int time)
