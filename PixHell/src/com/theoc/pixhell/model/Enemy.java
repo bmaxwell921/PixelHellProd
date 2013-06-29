786bc6cdb3c39033c491a4ef8873085462637b3c
Jatin Sharma <jsharma@iastate.edu>
2013-06-28 18:42:28 -0700

    Enemy Update Function

--- PixHell/src/com/theoc/pixhell/model/Enemy.java
+++ PixHell/src/com/theoc/pixhell/model/Enemy.java
@@ -1,14 +1,23 @@
 package com.theoc.pixhell.model;
 
+import java.nio.channels.ClosedByInterruptException;
+import java.util.ArrayList;
+import java.util.Queue;
+
 import android.graphics.Bitmap;
 import android.graphics.Point;
 
 public abstract class Enemy extends Ship {
-	
+	Queue<Point> Pathqueue;
+	Point TargetPos;
 	public Enemy(Bitmap image) {
 		this(image, defaultFireRate);
 	}
 	
+	public void setTargetPos(Point targetPos) {
+		TargetPos = targetPos;
+	}
+
 	public Enemy(Bitmap image, float fireRate) {
 		super(image, fireRate);
 	}
@@ -21,7 +30,39 @@ public abstract class Enemy extends Ship {
 		super(image, location, velocity, fireRate);
 	}
 	
-	protected void moveToLocation(Point dest) {
+	protected void moveToLocation(Point dest,float time) {
 		//Moves the current Enemy toward the given point
+		this.position.x =(int) (this.position.x+this.velocity.x*time);
+		this.position.y =(int) (this.position.y+this.velocity.y*time);
+			
 	}
+	public boolean closeTo(Point targetPos)
+    {
+		int xPos=targetPos.x-this.position.x;
+		int yPos=targetPos.y-this.position.y;
+		return false;
+        
+    }
+	@Override
+	public void update(float time) {
+		// TODO Auto-generated method stub
+		setTargetPos(Pathqueue.peek());
+		if(closeTo(TargetPos))
+		{
+			Pathqueue.poll();
+		}
+		moveToLocation(Pathqueue.peek(),time);
+		
+		
+	}
+
+	@Override
+	public boolean CollidesWith(GameObject gameObject) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	
+	
+
 }
