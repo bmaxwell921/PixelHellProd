7899172240f08c2c4ef43e9884bce8afff2199cf
rohan joshi <brel_rj@yahoo.com>
2013-07-21 18:59:15 -0700

    music changes

--- PixHell/src/com/theoc/pixhell/GameActivity.java
+++ PixHell/src/com/theoc/pixhell/GameActivity.java
@@ -15,6 +15,7 @@ import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,132 +23,136 @@ import android.view.MenuItem;
 import com.amazon.insights.ABTest;
 import com.amazon.insights.AmazonInsights;
 import com.amazon.insights.CustomEvent;
-import com.amazon.insights.*;
+import com.amazon.insights.Variation;
+import com.amazon.insights.VariationListener;
 import com.google.gson.Gson;
 import com.theoc.pixhell.db.StoreItemDTO;
 import com.theoc.pixhell.logic.AssetMap;
 import com.theoc.pixhell.manager.InputManager;
 import com.theoc.pixhell.manager.SoundManager;
-import com.theoc.pixhell.model.*;
+import com.theoc.pixhell.model.GameStartProperties;
+import com.theoc.pixhell.model.HealthConsumable;
+import com.theoc.pixhell.model.LevelObject;
+import com.theoc.pixhell.model.PersistentConsumable;
 import com.theoc.pixhell.utilities.Constants;
 import com.theoc.pixhell.utilities.GameState;
 import com.theoc.pixhell.utilities.Preferences;
 import com.theoc.pixhell.utilities.WeaponType;
 
-public class GameActivity extends Activity
-{
+public class GameActivity extends Activity {
 	public static final long GAME_RATE = 16;
 	public static final long FRAME_RATE = 32;
-	
-	private AssetManager 	assetMgr  = null;
-	
-	private Thread       gameThread   = null;
-	//private Thread dispThread;
-	private GameView     view         = null;
-	private LevelObject  model        = null;
+
+	private AssetManager assetMgr = null;
+
+	private Thread gameThread = null;
+	// private Thread dispThread;
+	private GameView view = null;
+	private LevelObject model = null;
 	private InputManager inputManager = null;
 	private SoundManager soundManager = null;
-	
+
 	int healthCount = 0;
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_game);
 		this.assetMgr = this.getAssets();
-		
+
 		// Configure the Amazon Insights SDK
-		AmazonInsights
-		    .withApplicationKey("f8f453bb8cf44935871480432bf58224")
-		    .withPrivateKey("M3TQ5VNO778YBH")
-		    .withContext(getApplicationContext())
-		    .initialize();
-		
-		AudioManager am   = (AudioManager) getSystemService(AUDIO_SERVICE);
-		SoundPool    sp   = new SoundPool(10, AudioManager.STREAM_MUSIC, 75);
-		MediaPlayer  mp   = new MediaPlayer();
-		this.inputManager = new InputManager((SensorManager) this.getSystemService(SENSOR_SERVICE));
+		AmazonInsights.withApplicationKey("f8f453bb8cf44935871480432bf58224")
+				.withPrivateKey("M3TQ5VNO778YBH")
+				.withContext(getApplicationContext()).initialize();
+
+		AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+		SoundPool sp = new SoundPool(10, AudioManager.STREAM_MUSIC, 75);
+		MediaPlayer mp = new MediaPlayer();
+		this.inputManager = new InputManager(
+				(SensorManager) this.getSystemService(SENSOR_SERVICE));
 		this.inputManager.setTiltSensitivity(this.getPresistentPref_Tilt());
-		
+
 		this.setSensorListners();
-		try
-		{
+		try {
 			Context context = this.getBaseContext();
 			AssetMap.initImage(this.assetMgr);
 			AssetMap.initSound(context, sp);
-			mp.setDataSource(this.assetMgr.openFd("mus/game_theme.mp3").getFileDescriptor()); 
+			mp.setDataSource(this.assetMgr.openFd("mus/game_theme.mp3")
+					.getFileDescriptor());
 			mp.prepare();
-		} catch (IOException e)
-		{
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		Log.i("custom", "sound manager called");
 		this.soundManager = new SoundManager(am, mp, sp);
-		this.soundManager.setVolumes(this.getPersistentPref_Music(), this.getPersistentPref_SFX());
-		
-		
+		this.soundManager.setVolumes(this.getPersistentPref_Music(),
+				this.getPersistentPref_SFX());
+
 		this.getPresistentPref();
 		HashMap<String, Integer> skuMap = this.getStoreData();
-		int health = skuMap.get(Constants.HEALTH_SKU);	
-		//int life = skuMap.get(Constants.LIFE_SKU);	
+		int health = skuMap.get(Constants.HEALTH_SKU);
+		// int life = skuMap.get(Constants.LIFE_SKU);
 		ArrayList<PersistentConsumable> defInv = new ArrayList<PersistentConsumable>();
 		for (int i = 0; i < health; i++) {
 			defInv.add(new HealthConsumable());
 		}
-		GameStartProperties props = new GameStartProperties(defInv, getPersistentPref_Diff(), getPersistentPref_Money());
-		
+		GameStartProperties props = new GameStartProperties(defInv,
+				getPersistentPref_Diff(), getPersistentPref_Money());
+
 		this.healthCount = health;
-		
+
 		Display display = getWindowManager().getDefaultDisplay();
 		Point size = new Point();
 		display.getSize(size);
 		int width = size.x;
 		int height = size.y;
-		
+
 		// link M-V
-		this.model = new LevelObject(width, height, props, this.inputManager, this.soundManager);
-		this.view = (GameView) this.findViewById(R.id.game_view_primary);	
+		this.model = new LevelObject(width, height, props, this.inputManager,
+				this.soundManager);
+		this.view = (GameView) this.findViewById(R.id.game_view_primary);
 		this.view.addInputManager(this.inputManager);
 		this.view.addModel(this.model);
-		
+
 		this.initThread();
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.game, menu);
 		return true;
 	}
-	
+
 	@Override
 	public void onStart() {
 		super.onStart();
 		if (this.gameThread != null) {
-			this.gameThread.start();		
+			this.gameThread.start();
 		}
 		this.soundManager.startTheme();
 		this.model.resume();
-		
+
 		CustomEvent.create("_session.start").record();
-		
-		
-		ABTest        
-	        .getVariationsByProjectNames("Session Start Weapon")
-	        .withVariationListener("Session Start Weapon", new VariationListener() {
-	            public void onVariationAvailable(Variation variation) {     
-	                final boolean b = variation.getVariableAsBoolean("startWithBullet", true);
-	                
-	                if (b) {
-	                	model.setPlayerWeapon(WeaponType.BULLET);
-	                } else {
-	                	model.setPlayerWeapon(WeaponType.TRI_BLASTER);
-	                }
-	                
-	            }
-	    });
+
+		ABTest.getVariationsByProjectNames("Session Start Weapon")
+				.withVariationListener("Session Start Weapon",
+						new VariationListener() {
+							public void onVariationAvailable(Variation variation) {
+								final boolean b = variation
+										.getVariableAsBoolean(
+												"startWithBullet", true);
+
+								if (b) {
+									model.setPlayerWeapon(WeaponType.BULLET);
+								} else {
+									model.setPlayerWeapon(WeaponType.TRI_BLASTER);
+								}
+
+							}
+						});
 	}
-	
+
 	@Override
 	public void onPause() {
 		super.onPause();
@@ -155,7 +160,7 @@ public class GameActivity extends Activity
 		this.model.pause();
 		CustomEvent.create("_session.pause").record();
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
@@ -163,14 +168,14 @@ public class GameActivity extends Activity
 		this.model.resume();
 		CustomEvent.create("_session.resume").record();
 	}
-	
-	@Override 
+
+	@Override
 	public void onRestart() {
 		super.onRestart();
 		this.soundManager.startTheme();
 		CustomEvent.create("_session.start").record();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
@@ -180,63 +185,65 @@ public class GameActivity extends Activity
 		this.soundManager.stopTheme();
 		CustomEvent.create("coins: " + this.model.getCoinNumber()).record();
 		CustomEvent.create("_session.stop").record();
-		
+
 		// MWAHAHAHAHAHAHAHAHAHAHA!!!!! - Force Custom Event Submission
 		Context context = getApplicationContext();
-		Intent intent = new Intent(context, com.amazon.insights.InsightsProcessingService.class);
+		Intent intent = new Intent(context,
+				com.amazon.insights.InsightsProcessingService.class);
 		intent.setAction("SubmitMeasurements");
 		intent.putExtra("force", true);
 		context.startService(intent);
-		android.util.Log.w("f8f453bb8cf44935871480432bf58224", "CODE TO FORCE SUBMISSION: I am a horrible person [^_^]");
+		android.util.Log.w("f8f453bb8cf44935871480432bf58224",
+				"CODE TO FORCE SUBMISSION: I am a horrible person [^_^]");
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	    	case R.id.weapons:
-	    		this.togglePause();
-	    		return true;
-	        case R.id.bullet:
-	        	CustomEvent.create("equip: bullet").record();
-	        	model.setPlayerWeapon(WeaponType.BULLET);
-	        	model.resume();
-	            return true;
-	        case R.id.missile:
-	        	CustomEvent.create("equip: missile").record();
-	        	model.setPlayerWeapon(WeaponType.MISSILE);
-	        	model.resume();
-	            return true;
-	        case R.id.tri_blaster:
-	        	CustomEvent.create("equip: tri blaster").record();
-	        	model.setPlayerWeapon(WeaponType.TRI_BLASTER);
-	        	model.resume();
-	            return true;
-	        case R.id.inventory:
-	        	this.togglePause();
-	        	return true;
-	        case R.id.health_pack:
-	        	if (healthCount > 0) {
-	        		CustomEvent.create("consumed: health").record();
-		        	useItem(Constants.HEALTH_SKU);
-		        	model.player.stats.restoreHealth();
-		        	model.resume();
-	        	}
-	        	return true;
-	        case R.id.store:
-	        	CustomEvent.create("store visited");
-	        	finish();
-	        	startActivity(new Intent(GameActivity.this, StoreActivity.class));
-	        	return true;
-	        case R.id.options:
-	        	finish();
-	        	startActivity(new Intent(GameActivity.this, OptionsActivity.class));
-	        	return true;
-	        default:
-	            return super.onOptionsItemSelected(item);
-	    }
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.weapons:
+			this.togglePause();
+			return true;
+		case R.id.bullet:
+			CustomEvent.create("equip: bullet").record();
+			model.setPlayerWeapon(WeaponType.BULLET);
+			model.resume();
+			return true;
+		case R.id.missile:
+			CustomEvent.create("equip: missile").record();
+			model.setPlayerWeapon(WeaponType.MISSILE);
+			model.resume();
+			return true;
+		case R.id.tri_blaster:
+			CustomEvent.create("equip: tri blaster").record();
+			model.setPlayerWeapon(WeaponType.TRI_BLASTER);
+			model.resume();
+			return true;
+		case R.id.inventory:
+			this.togglePause();
+			return true;
+		case R.id.health_pack:
+			if (healthCount > 0) {
+				CustomEvent.create("consumed: health").record();
+				useItem(Constants.HEALTH_SKU);
+				model.player.stats.restoreHealth();
+				model.resume();
+			}
+			return true;
+		case R.id.store:
+			CustomEvent.create("store visited");
+			finish();
+			startActivity(new Intent(GameActivity.this, StoreActivity.class));
+			return true;
+		case R.id.options:
+			finish();
+			startActivity(new Intent(GameActivity.this, OptionsActivity.class));
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
-	
+
 	private void togglePause() {
 		if (this.model.isPaused()) {
 			this.model.resume();
@@ -244,50 +251,51 @@ public class GameActivity extends Activity
 			this.model.pause();
 		}
 	}
-		
-	
+
 	// Register the event listener and sensor type.
-    private void setSensorListners()
-    {
-    	// Accelerometer (Tilt)
-        this.inputManager.sensorManager.registerListener(this.inputManager.mEventListener, this.inputManager.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 
-                SensorManager.SENSOR_DELAY_GAME);
-        // Magnetic Field (Compass Orientation)
-        this.inputManager.sensorManager.registerListener(this.inputManager.mEventListener, this.inputManager.sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), 
-                SensorManager.SENSOR_DELAY_GAME);
-    }
-	
+	private void setSensorListners() {
+		// Accelerometer (Tilt)
+		this.inputManager.sensorManager.registerListener(
+				this.inputManager.mEventListener,
+				this.inputManager.sensorManager
+						.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_GAME);
+		// Magnetic Field (Compass Orientation)
+		this.inputManager.sensorManager.registerListener(
+				this.inputManager.mEventListener,
+				this.inputManager.sensorManager
+						.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
 	private void initThread() {
 		// Set Update Thread
-		Runnable game_runner = 
-			new Runnable()
-			{
+		Runnable game_runner = new Runnable() {
 
-				@Override
-				public void run()
-				{
-					while (model.curGameState != GameState.GAME_OVER)
-					{
-						try {
-							Thread.sleep(GAME_RATE);
-							model.update(GAME_RATE);	
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
+			@Override
+			public void run() {
+				while (model.curGameState != GameState.GAME_OVER) {
+					try {
+						Thread.sleep(GAME_RATE);
+						model.update(GAME_RATE);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
 					}
-					CustomEvent.create("player death").record();
-					finish();
 				}
-			};
+				CustomEvent.create("player death").record();
+				finish();
+			}
+		};
 		this.gameThread = new Thread(game_runner);
 	}
-	
+
 	// PERSISTENCE =======================================================
-	
+
 	private HashMap<String, Integer> getStoreData() {
 		String wrapperStr = getPresistentPref();
 		if (wrapperStr != null) {
-			StoreItemDTO wrapper = new Gson().fromJson(wrapperStr, StoreItemDTO.class);
+			StoreItemDTO wrapper = new Gson().fromJson(wrapperStr,
+					StoreItemDTO.class);
 			HashMap<String, Integer> hashMap = wrapper.data;
 			return hashMap;
 		}
@@ -299,65 +307,60 @@ public class GameActivity extends Activity
 				MODE_PRIVATE).getString(
 				Preferences.persistantStorageIdentifier, null);
 	}
-	
+
 	private float getPresistentPref_Tilt() {
 		return getSharedPreferences(Preferences.applicationIdentifier,
-				MODE_PRIVATE).getFloat(
-				Preferences.tiltSensitivityIdentifier, 5f);
+				MODE_PRIVATE).getFloat(Preferences.tiltSensitivityIdentifier,
+				5f);
 	}
-	
+
 	private int getPersistentPref_Diff() {
 		return getSharedPreferences(Preferences.applicationIdentifier,
-				MODE_PRIVATE).getInt(
-				Preferences.difficultyIdentifier, 2);
+				MODE_PRIVATE).getInt(Preferences.difficultyIdentifier, 2);
 	}
-	
+
 	private int getPersistentPref_Money() {
 		return getSharedPreferences(Preferences.applicationIdentifier,
-				MODE_PRIVATE).getInt(
-				Preferences.walletIdentifier, 0);
+				MODE_PRIVATE).getInt(Preferences.walletIdentifier, 0);
 	}
-	
+
 	private float getPersistentPref_Music() {
 		return getSharedPreferences(Preferences.applicationIdentifier,
-				MODE_PRIVATE).getFloat(
-				Preferences.musicVolumeIdentifier, 5f);
+				MODE_PRIVATE).getFloat(Preferences.musicVolumeIdentifier, 5f);
 	}
-	
+
 	private float getPersistentPref_SFX() {
 		return getSharedPreferences(Preferences.applicationIdentifier,
 				MODE_PRIVATE).getFloat(
 				Preferences.soundEffectsVolumeIdentifier, 5f);
 	}
-	
+
 	/**
-	 * Update the persistent storage by 1.
-	 * User used 1 unit of stuff.
-	 * @param string 
+	 * Update the persistent storage by 1. User used 1 unit of stuff.
+	 * 
+	 * @param string
 	 */
 	public void useItem(String data) {
-		
-		//update
+
+		// update
 		HashMap<String, Integer> temp = getStoreData();
 		int newCount = temp.get(data) - 1;
 		temp.put(data, newCount);
-		
+
 		// push it
 		StoreItemDTO wrapper = new StoreItemDTO();
 		wrapper.data = temp;
 		String serializedMap = new Gson().toJson(wrapper);
 
 		getSharedPreferences(Preferences.applicationIdentifier, MODE_PRIVATE)
-				.edit()
-				.putString(Constants.HEALTH_SKU,
-						serializedMap).commit();
+				.edit().putString(Constants.HEALTH_SKU, serializedMap).commit();
 	}
-	
+
 	public void syncWallet() {
 		getSharedPreferences(Preferences.applicationIdentifier, MODE_PRIVATE)
 				.edit()
 				.putInt(Preferences.walletIdentifier,
 						this.model.getCoinNumber()).commit();
 	}
-	
+
 }
--- PixHell/src/com/theoc/pixhell/manager/SoundManager.java
+++ PixHell/src/com/theoc/pixhell/manager/SoundManager.java
@@ -5,45 +5,58 @@ import com.theoc.pixhell.logic.AssetMap;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.util.Log;
 
-public final class SoundManager
-{
+public final class SoundManager {
 	AudioManager audioManager = null;
-	MediaPlayer  mediaPlayer  = null;
-	SoundPool    soundPool    = null;
-	float        musicVol     = 1.0f;
-	float        sfxVol       = 1.0f;
-	
-	public SoundManager(AudioManager audioManager, MediaPlayer mediaPlayer, SoundPool soundPool) {
+	MediaPlayer mediaPlayer = null;
+	SoundPool soundPool = null;
+	float musicVol = 1.0f;
+	float sfxVol = 1.0f;
+
+	public SoundManager(AudioManager audioManager, MediaPlayer mediaPlayer,
+			SoundPool soundPool) {
 		this.soundPool = new SoundPool(4, AudioManager.STREAM_MUSIC, 100);
 		this.audioManager = audioManager;
-		this.mediaPlayer  = mediaPlayer;
-		this.soundPool    = soundPool;
-		
-		this.mediaPlayer.setVolume(musicVol, musicVol);
+		this.mediaPlayer = mediaPlayer;
+		this.soundPool = soundPool;
+
+		updateMediaPlayerVolume();
 		this.mediaPlayer.setLooping(true);
 	}
-	
+
 	public void setVolumes(float music, float sfx) {
 		this.musicVol = music;
 		this.sfxVol = sfx;
+		updateMediaPlayerVolume();
+		updateSoundEffectVolume(sfx);
+	}
+
+	private void updateSoundEffectVolume(float sfx) {
+		
+	}
+
+	private void updateMediaPlayerVolume(){
+		this.mediaPlayer.setVolume(musicVol, musicVol);
 	}
 	
 	public void startTheme() {
 		this.mediaPlayer.start();
 	}
-	
+
 	public void stopTheme() {
 		this.mediaPlayer.stop();
 	}
-	
+
 	public void pauseTheme() {
 		this.mediaPlayer.pause();
 	}
-	
-	public void resumeTheme() { }
-	
+
+	public void resumeTheme() {
+	}
+
 	public void playSoundEffect(int soundEffect) {
-		this.soundPool.play(AssetMap.getSoundID(soundEffect), sfxVol, sfxVol, 1, 0, 1f);
+		this.soundPool.play(AssetMap.getSoundID(soundEffect), sfxVol, sfxVol,
+				1, 0, 1f);
 	}
 }
